33072a0b1cb5960a671f9ecb6bc048e4ea5c4002
LE GARREC Vincent <github@le-garrec.fr>
2021-05-09 11:16:48 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # youtube-dl-cleanup
 Remove duplicated videos after download multiple times a playlist
+
+# Description
+
+When you want to update a playlist with youtube-dl, you simply run youtube-dl one more time.
+
+But after some years, youtube-dl will randomly decide to use another output format.
+So you will have some files like:
+
+```
+A Tutorial Introduction to C++11 & 14 Part 1-TK_SfTfxaxc.mkv
+A Tutorial Introduction to C++11 & 14 Part 1-TK_SfTfxaxc.mp4
+A Tutorial Introduction to C++11 & 14 Part 1-TK_SfTfxaxc.webm
+```
+
+But how to automatically remove old videos.
+
+On Windows, the modification date is the date of the video on the Youtube server.
+So based on the speed of the encoding, the new video may be the oldest.
+
+The idea is to use the creation date time.
+This information does not exist on Linux so this program is Windows only.
+
+# How to build
+
+You just need Visual Studio Community and open the project with CMake installed.
+
+# How to use it
+
+``youtubedl-cleanup.exe --dry-run -r path [more_path]``
+
+  * ``--dry-run``: do not erase, just show file that will be removed.
+  * ``-r``: recursively check. Usefull if you have multiple playlists in a same folder.
---
+++ src/youtubedl-cleanup.cpp
@@ -0,0 +1,111 @@
+﻿// YoutubeDlCleanUp.cpp : Defines the entry point for the application.
+//
+
+#include <filesystem>
+#include <iostream>
+#include <map>
+#include <optional>
+#include <queue>
+
+namespace fs = std::filesystem;
+
+#include <windows.h>
+
+int main(int argc, char* argv[])
+{
+	std::queue<fs::path> folders;
+
+	std::locale::global(std::locale(""));
+
+	bool r = false;
+	bool dry_run = false;
+	for (int i = 1; i < argc; i++)
+	{
+		if (std::string_view("-r") == argv[i])
+			r = true;
+		else if (std::string_view("--dry-run") == argv[i])
+			dry_run = true;
+		else if (!fs::is_directory(argv[i]))
+		{
+			std::cerr << "\"" << argv[i] << "\" is not a folder.\n";
+			return 1;
+		}
+		else
+			folders.push(argv[i]);
+	}
+
+	if (folders.empty())
+	{
+		std::cout << "Usage: youtubedl-cleanup.exe [--dry-run] [-r] path [more_path]\n";
+		return 1;
+	}
+
+	while (!folders.empty())
+	{
+		auto path = folders.front();
+		std::map<std::wstring, std::vector<fs::path>> file_ext;
+		for (const auto& entry : fs::directory_iterator(path))
+		{
+			try
+			{
+				if (entry.is_directory())
+				{
+					if (r)
+						folders.push(entry);
+				}
+				else
+				{
+					auto ext = entry.path().extension();
+					if (ext == ".mkv" || ext == ".mp4" || ext == ".webm")
+					{
+						auto root = (entry.path().parent_path() / entry.path().filename().replace_extension("")).wstring();
+						if (!file_ext.contains(root))
+							file_ext.insert({ root, {ext} });
+						else
+							file_ext[root].push_back(ext);
+					}
+				}
+			}
+			catch (const std::system_error&) {}
+		}
+
+		for (const auto& [file, ext] : file_ext)
+		{
+			if (ext.size() < 2)
+				continue;
+
+			std::map<std::uint64_t, std::wstring> times;
+
+			for (const auto& exti : ext)
+			{
+				HANDLE hFile;
+				FILETIME ftCreate;
+				std::wstring filei = file + exti.wstring();
+				hFile = CreateFileW(filei.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+				if (hFile == INVALID_HANDLE_VALUE)
+				{
+					std::wcerr << "Could not open file \"" << filei << "\", error " << GetLastError() << "\n";
+					return 1;
+				}
+
+				if (!GetFileTime(hFile, &ftCreate, NULL, NULL))
+				{
+					std::wcerr << "Corrumpted file \"" << filei << "\"\n";
+					return 1;
+				}
+
+				CloseHandle(hFile);
+
+				times.insert({ (static_cast<std::uint64_t>(ftCreate.dwHighDateTime) << 32L) + ftCreate.dwLowDateTime, exti.wstring() });
+			}
+			std::wcout << file.c_str() << times.begin()->second << std::endl;
+			if (!dry_run)
+				fs::remove(file + times.begin()->second);
+		}
+
+		folders.pop();
+	}
+
+	return 0;
+}
